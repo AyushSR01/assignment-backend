0e8b3bf845bcc6d7c6d400fc528b4c85028e6744
AyushSR01 <you@example.com>
2025-07-04 23:07:46 +0530

    ww

--- server.js
+++ server.js
@@ -19,16 +19,8 @@ const allowedOrigins = [
 ];
 
 app.use(cors({
-  origin: function (origin, callback) {
-    if (!origin || allowedOrigins.includes(origin)) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  },
+  origin: process.env.FRONT_END_URL,
   credentials: true,
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization']
 }));
 
 
